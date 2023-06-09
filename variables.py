6da6f2223118c40f9ac6312d91eba7e9c7e16be1
Guadalupe <guadalupe04.lc@gmail.com>
2023-06-09 13:26:02 -0600

    variables

---
+++ variables.py
@@ -0,0 +1,20 @@
+# lenguaje no tipado
+name="Montse valencia"
+age = 21
+height = 165
+is_student = True
+children = ["yes"]
+msj = "te quiero mucho bebe"
+
+print (type(name))
+print (type(age))
+print (type(height))
+print (type(is_student))
+print (type(children))
+
+print (name)
+print (age)
+print (height)
+print (is_student)
+print (children)
+print (msj)
