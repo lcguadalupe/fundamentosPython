1b3b43721f24f6c9548cf2fd2bf8372b61471152
Guadalupe <guadalupe04.lc@gmail.com>
2023-06-09 13:59:38 -0600

    04 operadores

---
+++ operadores.py
@@ -0,0 +1,17 @@
+#operadores matematicas
+# + - * %
+# / division float
+# // division entera
+# ** potencia 
+print(2+2)
+print(2-2)
+print(2*2)
+print(2%2)
+print(2/2)
+print(2//2)
+print(2**2)
+
+# operadores para strings
+# + concatena * replica
+print("hola" + "mundo!")
+print("hola\n" * 4)
