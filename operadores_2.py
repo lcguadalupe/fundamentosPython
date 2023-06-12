17b8372fb9744985b8ea7098ff1c2c7fb4f1181c
Guadalupe <guadalupe04.lc@gmail.com>
2023-06-12 09:55:09 -0600

    05 operadores logicos y booleanos

---
+++ operadores_2.py
@@ -0,0 +1,23 @@
+# operadores booleanos
+# > >=
+print(10>10)
+print(10>=10)
+
+# < <=
+print(10<10)
+print(10<=10)
+# ==
+print(10==10)
+# !=
+print(10!=10)
+
+# operacdores logicos
+# and (true if all true)
+print(True and True)
+print(True and False)
+# or (true if al least one true)
+print(True or True)
+print(True or False)
+# not (not true)
+print(not True)
+print(not False)
