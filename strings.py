46b96cb0912bb7f6cfc898a8d8edde99c7b7ccd2
Guadalupe <guadalupe04.lc@gmail.com>
2023-06-13 14:16:33 -0600

    07 strings

---
+++ strings.py
@@ -0,0 +1,30 @@
+my_string = " hOla mUnDo! "
+
+# mayusculas 
+print(my_string.upper())
+
+# minusculas 
+print(my_string.lower())
+
+# longitud de string
+print(len(my_string))
+
+# quitar espacios inicio y fin 
+print(my_string.strip())
+
+my_string = "este es un nuevo texto"
+
+# indexing and slicing 
+print(my_string[0])
+print(my_string[3])
+print(my_string[6])
+
+print(my_string[-1])
+print(my_string[-2])
+
+#SLICING
+# [inicio:fin]
+print(my_string[0:5])
+print(my_string[2:5])
+print(my_string[5:])
+
